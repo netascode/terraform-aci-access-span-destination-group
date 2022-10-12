2eafad550176f2f4abf6b58410d9f6df4d54347c
Jorge Gomez Velasquez <jorgegomez2307@hotmail.com>
2022-10-12 01:48:16 -0500

    Add module (#1)

--- tests/destination_path_channel/test_destination_path_channel.tf
+++ tests/destination_path_channel/test_destination_path_channel.tf
@@ -16,11 +16,14 @@ terraform {
 module "main" {
   source = "../.."
 
-  name = "TEST_MINIMAL"
+  name    = "TEST_PATH_CHANNEL"
+  mtu     = 9000
+  node_id = 101
+  channel = "PC1"
 }
 
 data "aci_rest_managed" "spanDestGrp" {
-  dn = "uni/infra/destgrp-TEST_MINIMAL"
+  dn = "uni/infra/destgrp-TEST_PATH_CHANNEL"
 
   depends_on = [module.main]
 }
@@ -31,7 +34,7 @@ resource "test_assertions" "spanDestGrp" {
   equal "name" {
     description = "name"
     got         = data.aci_rest_managed.spanDestGrp.content.name
-    want        = "TEST_MINIMAL"
+    want        = "TEST_PATH_CHANNEL"
   }
 
   equal "descr" {
@@ -42,7 +45,7 @@ resource "test_assertions" "spanDestGrp" {
 }
 
 data "aci_rest_managed" "spanDest" {
-  dn = "${data.aci_rest_managed.spanDestGrp.id}/dest-TEST_MINIMAL"
+  dn = "${data.aci_rest_managed.spanDestGrp.id}/dest-TEST_PATH_CHANNEL"
 
   depends_on = [module.main]
 }
@@ -53,12 +56,28 @@ resource "test_assertions" "spanDest" {
   equal "name" {
     description = "name"
     got         = data.aci_rest_managed.spanDest.content.name
-    want        = "TEST_MINIMAL"
+    want        = "TEST_PATH_CHANNEL"
   }
 
   equal "descr" {
     description = "descr"
     got         = data.aci_rest_managed.spanDest.content.descr
     want        = ""
+  }
+}
+
+data "aci_rest_managed" "spanRsDestPathEp" {
+  dn = "${data.aci_rest_managed.spanDest.id}/rsdestPathEp-[topology/pod-1/paths-101/pathep-[PC1]]"
+
+  depends_on = [module.main]
+}
+
+resource "test_assertions" "spanRsDestPathEp" {
+  component = "spanRsDestPathEp"
+
+  equal "tDn" {
+    description = "tDn"
+    got         = data.aci_rest_managed.spanRsDestPathEp.content.tDn
+    want        = "topology/pod-1/paths-101/pathep-[PC1]"
   }
 }
